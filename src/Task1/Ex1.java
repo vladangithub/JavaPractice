aba4642d1e9b5a1b4cde0448fce137f8b0dcc0d3
azesm <azesm@yahoo.com>
2024-05-29 19:30:40 +0300

    First

---
+++ src/Task1/Ex1.java
@@ -0,0 +1,13 @@
+package Task1;
+
+/*
+* Вывести на экран слово “JAVA”, в строку, чтобы оно повторилось 10 раз, используя
+цикл while.
+Вывод в консоль должен быть таким:
+JAVA JAVA JAVA JAVA JAVA JAVA JAVA JAVA JAVA JAVA*/
+
+public class Ex1 {
+    public static void main(String[] args) {
+
+    }
+}
