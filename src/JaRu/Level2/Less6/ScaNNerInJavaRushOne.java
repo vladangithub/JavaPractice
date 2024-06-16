460fbc998019360846caa88f72cdb2f60172a572
azesm <azesm@yahoo.com>
2024-06-16 13:49:26 +0300

    jaru2_Класс Scanner в Java_ javaRush

---
+++ src/JaRu/Level2/Less6/ScaNNerInJavaRushOne.java
@@ -0,0 +1,24 @@
+package JaRu.Level2.Less6;
+import java.util.Scanner;
+
+public class ScaNNerInJavaRushOne {
+    public static void main(String[] args) {
+//        Scanner scanner = new Scanner("Люблю тебя, Петра творенье,\n" +
+//                "Люблю твой строгий, стройный вид,\n" +
+//                "Невы державное теченье,\n" +
+//                "Береговой ее гранит");
+//        String s = scanner.nextLine();
+//        System.out.println(s);
+//        s = scanner.nextLine();
+//        System.out.println(s);
+//        s = scanner.nextLine();
+//        System.out.println(s);
+//        s = scanner.nextLine();
+//        System.out.println(s);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите число: ");
+
+        int num = sc.nextInt();
+        System.out.println("Ваше введенное число: " + num);
+    }
+}
