6d27ef6b16241bddd1e2bff0e2f9e0c30445a757
azesm <azesm@yahoo.com>
2024-06-17 18:22:40 +0300

    Level3.Less4.IfElseJR

---
+++ src/JaRu/Level3/Less5/ThreeNumber.java
@@ -0,0 +1,17 @@
+package JaRu.Level3.Less5;
+
+import java.util.Scanner;
+
+public class ThreeNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+        int num3 = scanner.nextInt();
+
+        boolean q1 = (a == b);
+        boolean q2 = (a == c);
+        boolean q3 = (b == c);
+
+    }
+}
