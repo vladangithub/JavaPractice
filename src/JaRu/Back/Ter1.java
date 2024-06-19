18e41b9ff78d5ed29772c0d8c6078e310884dd49
azesm <azesm@yahoo.com>
2024-06-19 15:21:42 +0300

    JaRu.Level3.Less7.Ter1

---
+++ src/JaRu/Back/Ter1.java
@@ -0,0 +1,70 @@
+package JaRu.Back;
+
+import java.util.Scanner;
+import java.util.Scanner;
+
+public class Ter1 {
+    /*
+У нас есть программа, которая считывает с клавиатуры два числа и выводит на экран большее из них. Если числа одинаковые, то выводится любое.
+Перепиши программу с использованием тернарного оператора, чтобы ее функционал остался без изменений.
+
+Требования:
+•	Программа должна считывать два числа c клавиатуры.
+•	Вместо оператора if-else в программе нужно использовать тернарный оператор.
+•	Функционал программы не должен измениться.
+ */
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int numberA = scanner.nextInt();
+        int numberB = scanner.nextInt();
+
+//        if (numberA > numberB) {
+//            System.out.println(numberA);
+//        } else {
+//            System.out.println(numberB);
+//        }
+//        boolean numMax = (numberA > numberB);
+//        boolean numMax ? System.out.println(numberA) : System.out.println(numberB);
+
+
+//       (НЕВЕРНО НУЖНО ПЕРЕДЕЛАТЬ!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!)
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//        System.out.println(numberA > numberB ? numberA : numberB);
+//
+    }
+}
+
