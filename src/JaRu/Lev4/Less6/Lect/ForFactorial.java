2971ec56a55b9651c0fde9538b6288dbdcce3564
azesm <azesm@yahoo.com>
2024-06-28 15:21:23 +0300

    JaRu.Level4.Less6.Ex.factorial

---
+++ src/JaRu/Lev4/Less6/Lect/ForFactorial.java
@@ -0,0 +1,11 @@
+package JaRu.Lev4.Less6.Lect;
+
+public class ForFactorial {
+    public static void main(String[] args) {
+        int f = 1;
+        for (int i = 1; i <= 10; i++) {
+            f *= i;
+        }
+        System.out.println(f);
+    }
+}
