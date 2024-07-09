bf26442d5e760374aae186b5a4050ac5e83f1500
azesm <azesm@yahoo.com>
2024-07-09 13:53:42 +0300

    JaRu.Level6.Less1.ArrBlock

---
+++ src/JaRu/Lev6/Less1/IntDoubleArray.java
@@ -0,0 +1,8 @@
+package JaRu.Lev6.Less1;
+
+public class IntDoubleArray {
+    public static void main(String[] args) {
+        int[] intArray = new int[10];
+        double[] doubleArray = new double[10];
+    }
+}
