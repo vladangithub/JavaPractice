faffeb2ac293e418082572dc42bd76133d18698a
azesm <azesm@yahoo.com>
2024-09-05 02:33:52 +0300

    DevOnAc.P07Cycles.Lec.For

--- src/DevOnAc/P07Cycles/Lec/Cicles.java
+++ src/DevOnAc/P07Cycles/Lec/Cicles.java
@@ -2,20 +2,18 @@ package DevOnAc.P07Cycles.Lec;
 
 public class Cicles {
     public static void main(String[] args) {
-        System.out.println("hello, world");
-        System.out.println("hello, world");
-        System.out.println("hello, world");
-        System.out.println("hello, world");
-        System.out.println("hello, world");
-        System.out.println("hello, world");
-        System.out.println("hello, world");
-        System.out.println("hello, world");
-        System.out.println("hello, world");
-        System.out.println("hello, world");
-        System.out.println();
-        for (int i = 0; i <= 5; i++) {
-            System.out.println("hello, world");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+
+        System.out.println("I love JAVA");
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Hello World");
+
         }
-        System.out.println("hello, Bobb");
     }
+
 }
---
+++ src/DevOnAc/P07Cycles/Lec/For.java
@@ -0,0 +1,7 @@
+package DevOnAc.P07Cycles.Lec;
+
+public class For {
+    public static void main(String[] args) {
+        System.out.println("Before for");
+    }
+}
