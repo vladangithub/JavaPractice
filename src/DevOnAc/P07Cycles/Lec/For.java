a9d24b9e1eacbf88c6863eb030a5d3591a8c0dd3
azesm <azesm@yahoo.com>
2024-09-05 02:55:46 +0300

    DevOnAc.P07Cycles.Lec.ForExSecond

--- src/DevOnAc/P07Cycles/Lec/For.java
+++ src/DevOnAc/P07Cycles/Lec/For.java
@@ -3,5 +3,11 @@ package DevOnAc.P07Cycles.Lec;
 public class For {
     public static void main(String[] args) {
         System.out.println("Before for");
+
+        for (var i = 1; i <= 5; i++){
+            System.out.println(i);
+        }
+
+            System.out.println("After for");
     }
 }
---
+++ src/DevOnAc/P07Cycles/Lec/ForExSecond.java
@@ -0,0 +1,20 @@
+package DevOnAc.P07Cycles.Lec;
+
+public class ForExSecond {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.print(i + 1 + " ");
+        }
+        System.out.println("\n");
+
+        for (int i = 5; i > 0; i--) {
+            System.out.print(i + " ");
+        }
+        System.out.println("\n");
+
+        for (int i = 4; i >= 0; i--) {
+            System.out.print(i + 1 + " ");
+        }
+        System.out.println("\n");
+    }
+}
